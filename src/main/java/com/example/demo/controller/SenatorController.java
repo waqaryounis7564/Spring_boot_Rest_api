9a6746635f075a467ace5066661cb0a1c0ef82ef
Waqar <waqar.younas7564@gmail.com>
2021-04-28 12:21:12 +0500

    scheduling enable with get request over senator service

--- src/main/java/com/example/demo/controller/SenatorController.java
+++ src/main/java/com/example/demo/controller/SenatorController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.service.SenatorData;
 import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Date;
 
 @RestController
 public class SenatorController {
     private SenatorData senatorData;
+    private static final Logger logger = LoggerFactory.getLogger(SenatorController.class);
 
     @Autowired
     SenatorController(SenatorData senatorData) {
@@ -22,14 +26,17 @@ public class SenatorController {
     }
 
     @GetMapping(path = "/sd", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 900000)
     ResponseEntity<JSONObject> getStart() {
+        System.out.println("Senator API starts");
         JSONObject jsonObject = null;
         try {
             jsonObject = senatorData.scrapeData(false);
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        System.out.println(java.time.LocalTime.now());
+        logger.error(String.valueOf(java.time.LocalTime.now()));
         return new ResponseEntity<>(jsonObject, HttpStatus.OK);
     }
 }
